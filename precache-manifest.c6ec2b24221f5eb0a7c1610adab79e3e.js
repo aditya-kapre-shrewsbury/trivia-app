8e3560d46f8e54f40b303f650bc1cb4d16c1b024
Aditya <aditya14641@gmail.com>
2023-07-19 01:02:32 -0400

    Updates

---
+++ precache-manifest.c6ec2b24221f5eb0a7c1610adab79e3e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f1c50723b9191fffa3d0",
+    "url": "/trivia-app/static/css/main.d0fb0c6f.chunk.css"
+  },
+  {
+    "revision": "f1c50723b9191fffa3d0",
+    "url": "/trivia-app/static/js/main.f1c50723.chunk.js"
+  },
+  {
+    "revision": "4587b35573b7dda0a77d",
+    "url": "/trivia-app/static/js/1.4587b355.chunk.js"
+  },
+  {
+    "revision": "f4498a76585fcfffc0e9",
+    "url": "/trivia-app/static/js/2.f4498a76.chunk.js"
+  },
+  {
+    "revision": "7fe24bf00c2c257cf46e",
+    "url": "/trivia-app/static/js/runtime~main.7fe24bf0.js"
+  },
+  {
+    "revision": "570dcf4d820bbbda7b4eaf4fd2191885",
+    "url": "/trivia-app/index.html"
+  }
+];
